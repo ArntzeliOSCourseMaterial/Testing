48da3aa9bcbd68a18a31973cb00cfa5922ffbb81
Eliot Arntz <arntzel@gmail.com>
2013-08-11 09:19:39 -0400

    Initial Commit

---
+++ ObjectRecipes/GAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GAAppDelegate.h
+//  ObjectRecipes
+//
+//  Created by Eliot Arntz on 8/11/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ObjectRecipes/GAViewController.h
@@ -0,0 +1,13 @@
+//
+//  GAViewController.h
+//  ObjectRecipes
+//
+//  Created by Eliot Arntz on 8/11/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GAViewController : UIViewController
+
+@end
